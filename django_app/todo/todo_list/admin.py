fdefbaff943d51ec8af24bd91d4b6848e35d7b6b
nabila <nabilatajrin@gmail.com>
2020-02-10 13:22:32 +0600

    update

--- django_app/todo/todo_list/admin.py
+++ django_app/todo/todo_list/admin.py
@@ -2,5 +2,7 @@
 # with the Django admin application. admin.py − This file helps you make the app modifiable in the admin interface.
 
 from django.contrib import admin
+from . models import List
 
 # Register your models here.
+admin.site.register(List)
