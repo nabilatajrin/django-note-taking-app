58ec0530100633b3edb4ab5e6cd39337b2fdb02e
nabila <nabilatajrin@gmail.com>
2020-02-26 12:26:53 +0600

    12:27PM 26FEB2020 update

--- django_app/todo/todo_list/models.py
+++ django_app/todo/todo_list/models.py
@@ -8,6 +8,7 @@ class List(models.Model):
     item = models.CharField(max_length = 200)
     completed = models.BooleanField(default=False)
 
+    # Pythonic way to convert Python objects into strings by using __str__.
     def __str__(self):
         return self.item + ' | ' + str(self.completed)
 
--- django_app/todo/todo_list/serializers.py
+++ django_app/todo/todo_list/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from todo_list.models import List
 
 
-class NoteSerializer(serializers.Serializer):
-    """Serializers a name field for testing our APIView"""
-    class Meta:
-        model = List
-        fields = ('id', 'todo_text', 'done')
+# class NoteSerializer(serializers.Serializer):
+#     """Serializers a name field for testing our APIView"""
+#     class Meta:
+#         model = List
+#         fields = ('id', 'todo_text', 'done')
 
 
 class NoteApiSerializer(serializers.ModelSerializer):
